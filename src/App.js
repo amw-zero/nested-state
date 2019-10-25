a6f49fc7c13c32f2ed598ad4dc06458b20cc8c31
Alex Weisberger <alex.weisberger@viewthespace.com>
2019-10-25 09:31:56 -0400

    nested state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let initialState = {
+  user: {
+    email: 'user@email.com'
+  }
+};
+
 function App() {
+  let [state, setState] = useState(initialState);
+
+  function changeEmail() {
+    setState({ 
+      user: {
+        email: 'other@email.com'
+      }
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={changeEmail}>Change Email</button>
+      { state.user.email }
     </div>
   );
 }
